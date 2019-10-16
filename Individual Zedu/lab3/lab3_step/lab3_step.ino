4d343112010952a326d60e44587d746990fe4ed2
zedu14 <zelopes195@gmail.com>
2019-10-16 14:42:10 +0100

    lab3 + resultados

--- Individual Zedu/lab3/lab3_step/lab3_step.ino
+++ Individual Zedu/lab3/lab3_step/lab3_step.ino
@@ -1,1 +1,73 @@
 
+
+//read from ldr
+const int analogIn = A0;
+float LDRvalue = 0.0;
+float VR = 0.0;
+float R = 0.0;
+float b = 4.9;
+float m = -0.84;
+float R1 = 10000.0;
+float L = 0.0;
+float V = 5;
+
+//write to led
+const int digitalOutLED = 3;
+int LEDIntens = 0;
+int totalCiclo = 255;
+int duty = 0;
+int duty_init = 0;
+float duty_float = 0.0;
+float on = 0;
+String readIn ="";
+
+int i = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(analogIn, INPUT);
+  pinMode(digitalOutLED, OUTPUT);
+}
+
+void loop () {
+
+  Serial.println("Insira qual step");
+  while(readIn == ""){
+    readIn=Serial.readString();
+    duty = duty_init;
+    analogWrite(digitalOutLED, 0);  
+  }
+
+   duty = readIn.toInt();
+  readIn = "";
+  Serial.print("duty: ");
+  Serial.println(duty);
+  
+  duty_float = duty / 100.0;
+  on = totalCiclo*duty_float;
+
+
+  analogWrite(digitalOutLED, on);  
+
+  Serial.println("Out Lux: ");
+  for (i=0;i<100;i++){
+    LDRvalue = analogRead(analogIn);
+    VR = 5- ( LDRvalue / 1023)*5 ;
+    R = (R1*VR) / (V-VR);
+    L = pow(10, (log(R) / log(10) - b)/(m));
+    
+    Serial.println(L); 
+    delayMicroseconds(1); 
+  }
+ 
+    
+   
+  
+  
+  
+}
+
+
+
+
