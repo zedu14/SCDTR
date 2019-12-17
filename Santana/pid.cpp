d0c2f6892883291e3ec3df843b20c9ead193dedb
bernardosanti <bernardo.s.1997@gmail.com>
2019-12-17 15:57:16 +0000

    codigo_atual

--- Santana/pid.cpp
+++ Santana/pid.cpp
@@ -3,29 +3,22 @@
 using namespace std;
 const int analogOutPin = 9;
 
-void pid::init(float p, float i, float d, float _T, float _a)
+void pid::init(float p, float i, float _T)
 {
-    ep = yp = ip = dp = 0.0f;
-    k1=kp=p; ki=i; kd=d; T=_T; a=_a;
+    ip = 0.0f;
+    k1=kp=p; ki=i; T=_T;
     k2=kp*ki*T/2;
-    if (kd < 0.000001f)
-        derivative = false;
-    else {
-        float den = kd+a*T;
-        //warning: should check den
-        k3=kd/den;
-        k4=kp*kd*a/den;
-    }
+
+
 }
 
 float pid::calc(float ref, float y, int u_feedforward)
 {
     float e = ref - y;
     float p = k1*e;
-    float i = ip + k2*(e+ep);
-    float d = 0;
-    if(derivative)
-        d = k3*dp - k4*(y-yp);
+    float i = ip + k2*(e);
+
+
 
     //anti-windup
     if(p+i+u_feedforward > 250)
@@ -33,9 +26,9 @@ float pid::calc(float ref, float y, int u_feedforward)
     else if (p+i+u_feedforward < 0)
       i = -(-p+u_feedforward);
 
-    analogWrite(analogOutPin, constrain(p+i+d+u_feedforward,0,250));
-    yp = y; ip = i; dp = d; ep = e;
-    return p+i+d+u_feedforward;
+    analogWrite(analogOutPin, constrain(p+i+u_feedforward,0,250));
+    ip = i;
+    return p+i+u_feedforward;
 }
 void pid::imprimir()
 {
@@ -43,10 +36,6 @@ void pid::imprimir()
     Serial.print(kp);
     Serial.print(" ki: ");
     Serial.print(ki);
-    Serial.print(" kd: ");
-    Serial.print(kd);
     Serial.print(" T: ");
     Serial.print(T);
-    Serial.print(" a: ");
-    Serial.println(a);
 }
--- Santana/pid.h
+++ Santana/pid.h
@@ -1,19 +1,13 @@
-//
-// Created by Alexandre Bernardino on 20/10/2019.
-//
-
 #ifndef PID_H
 #define PID_H
 #include <Arduino.h>
 using namespace std;
 class pid  {
-    bool derivative;
-    float kp, ki, kd, T, a;
-    float k1, k2, k3, k4;
-    float ep, yp, ip, dp;
+    float kp, ki, T;
+    float k1, k2;
+    float ip;
 public:
-    void init(float kp, float ki,
-              float kd, float T, float a);
+    void init(float kp, float ki, float T);
     float calc(float ref, float y, int u_feedforward);
     void imprimir();
 };
